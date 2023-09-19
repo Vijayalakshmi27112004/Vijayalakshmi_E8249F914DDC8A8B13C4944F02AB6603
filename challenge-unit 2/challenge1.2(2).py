f9557e1790463f7842d8d03f549867178b7ff04c
Vijayalakshmi27112004 <144519047+Vijayalakshmi27112004@users.noreply.github.com>
2023-09-19 21:03:25 +0530

    Delete challenge-unit 2/challenge1.2(2).py

--- challenge-unit 2/challenge1.2(2).py
+++
@@ -1,9 +0,0 @@
-def checkleap(year):
-    if ((year%400==0)or
-       (year%100!=0)and
-       (year%4==0)):
-       print("Given year is a leap year")
-    else:
-        print("Given year is not leap year")
-year = int(input("Enter the number :"))
-checkleap(year)
